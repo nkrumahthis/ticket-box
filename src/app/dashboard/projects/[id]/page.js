1638f54902ba0c3de73e841924e920e6847a1709
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-07-03 09:52:04 +0000

    project dynamic page route

---
+++ src/app/dashboard/projects/[id]/page.js
@@ -0,0 +1,3 @@
+export default function Page({ params }) {
+  return <div>Project Id: {params.id}</div>
+}
--- src/app/dashboard/projects/page.js
+++ src/app/dashboard/projects/page.js
@@ -11,23 +11,23 @@ export default async function Page() {
   return (
     <div>
       <div className="flex justify-between container mx-auto p-8 w-3/4">
-        <h1 className="text-4xl">Projects</h1>
-        <Link className="bg-blue-800 text-white py-2 px-3 rounded text-lg" href={"dashboard/projects/create"}>Create</Link>
+        <h1 className="text-2xl">Projects</h1>
+        <Link className="bg-blue-800 text-white py-2 px-3 rounded text-m" href={"dashboard/projects/create"}>Create</Link>
       </div>
       <div className="border-t-4 border-blue-500">
-        <table className="table-fixed w-3/4 mt-4 mx-auto">
-          <thead className="table-header-group">
+        <table className="table-auto w-3/4 mt-4 mx-auto p-3 text-left">
+          <thead className="table-header-group border-b">
             <tr className="table-row">
-              <th className="table-cell">Name</th>
-              <th className="table-cell">Owner</th>
+              <th className="table-cell p-5">Name</th>
+              <th className="table-cell p-5">Owner</th>
             </tr>
           </thead>
           <tbody className="table-row-group">
             {
               projects.map((row, index) => (
-                <tr key={index} className="table-row hover:bg-slate-800">
-                  <td className="table-cell">{row.name}</td>
-                  <td className="table-cell">{row.owner.name}</td>
+                <tr key={index} className="table-row hover:bg-slate-800 p-3">
+                  <Link className="text-blue-400 hover:underline" href={"projects/" + row.id}><td className="table-cell p-4">{row.name}</td></Link>
+                  <td className="table-cell p-4">{row.owner.name}</td>
                 </tr>
               ))
             }
